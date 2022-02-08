375097f4334abff98cba3c22eb84b3d94e26e8ef
ArnaudMarcel <arnaud.lasv@gmail.com>
2022-02-08 16:36:28 +0100

    tf_idf

--- light_request.sh
+++ light_request.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "--------START PROCESSING AT: $(date "+%Y-%m-%d %H:%M:%S")--------"
 
-query=''
+query='cook'
 fields='created_at,public_metrics,author_id,lang'
-bearerToken=''
+bearerToken='1490688234296381440-VT69KI8vI6uB4heacyOiidLFdSWGrv'
 maxresults=100
 echo "===> couting output"
 curl -X GET -H "Authorization: Bearer ${bearerToken}" "https://api.twitter.com/2/tweets/counts/recent?query=${query}" > count_tweet.json
