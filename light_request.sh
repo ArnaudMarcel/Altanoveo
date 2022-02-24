9d4d31a7c281f7d7cd93ab97ee02210498124ece
ArnaudMarcel <arnaud.lasv@gmail.com>
2022-02-24 18:33:00 +0100

    ajout de tweets et arbre

--- light_request.sh
+++ light_request.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "--------START PROCESSING AT: $(date "+%Y-%m-%d %H:%M:%S")--------"
 
-query='cook'
+query='fish'
 fields='created_at,public_metrics,author_id,lang'
-bearerToken='1490688234296381440-VT69KI8vI6uB4heacyOiidLFdSWGrv'
+bearerToken='AAAAAAAAAAAAAAAAAAAAAINlZgEAAAAAWkessX16ldt0dpRmEvI3HSS0RV0%3DXotNmRMj5yL8Cw7fRWIEvblyH7bRV3OsmuXO4Ir6vwaiThCl2Y'
 maxresults=100
 echo "===> couting output"
-curl -X GET -H "Authorization: Bearer ${bearerToken}" "https://api.twitter.com/2/tweets/counts/recent?query=${query}" > count_tweet.json
+curl -X GET -H "Authorization: Bearer ${bearerToken}" "https://api.twitter.com/2/tweets/counts/recent?query=${query}"
 echo "===> start request"
-curl -X GET -H "Authorization: Bearer ${bearerToken}" "https://api.twitter.com/2/tweets/search/recent?tweet.fields=${fields}&max_results=${maxresults}&query=${query}" > output_tweet.json
+curl -X GET -H "Authorization: Bearer ${bearerToken}" "https://api.twitter.com/2/tweets/search/recent?tweet.fields=${fields}&max_results=${maxresults}&query=${query}" > without_fire/output_${query}_tweet.json
 
 echo "--------START FINISHED AT: $(date "+%Y-%m-%d %H:%M:%S")--------"
